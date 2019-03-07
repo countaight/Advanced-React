cee131f739b2703b3f351d525c41354f9ed5bce4
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2019-03-07 12:20:48 -0600

    Add single item query and update item mutation to schema and resolvers.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,19 @@ const Mutations = {
 		}, info);
 
 		return item;
+	},
+	updateItem(parent, args, ctx, info) {
+		// take a copy of the updates
+		const updates = { ...args };
+		// remove gthe ID from the updates
+		delete updates.id;
+		// run the update method
+		return ctx.db.mutation.updateItem({
+			data: updates,
+			where: {
+				id: args.id
+			},
+		}, info);
 	}
 };
 
