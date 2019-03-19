314e380bf51c02f79f3faf495e20b26a3d3df9c7
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2019-03-19 15:43:26 -0500

    Add user type and allow password hashing

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		// TODO: check if they are logged in
@@ -30,7 +33,29 @@ const Mutations = {
 		// 2. Check if they own the item or have permissions
 		// 3. Delete it
 		return ctx.db.mutation.deleteItem({ where }, info);
-	}
+	},
+	async signup(parent, args, ctx, info) {
+		args.email = args.email.toLowerCase();
+		// Hash user's password
+		const password = await bcrypt.hash(args.password, 10);
+		// Create user
+		const user = await ctx.db.mutation.createUser({
+			data: {
+				...args,
+				password,
+				permissions: { set: ['USER'] }
+			}
+		}, info);
+		// Create JWT for them
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// Set token to the cookie in the response
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365, //1 year cookie
+		});
+		// Return user
+		return user;
+	},
 };
 
 module.exports = Mutations;
