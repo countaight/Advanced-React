0937746ffe045c88932a9daa73fc80774d4fb1af
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2019-03-22 13:41:14 -0500

    Create middleware to retrieve current user.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,11 +4,15 @@ const Query = {
 	items: forwardTo('db'),
 	item: forwardTo('db'),
 	itemsConnection: forwardTo('db'),
-	// async items(parent, args, ctx, info) {
-	// 	const items = await ctx.db.query.items();
+	me(parent, args, ctx, info) {
+		if (!ctx.request.userId) {
+			return null;
+		}
 
-	// 	return items;
-	// }
+		return ctx.db.query.user({
+			where: { id: ctx.request.userId }
+		}, info);
+	},
 };
 
 module.exports = Query;
